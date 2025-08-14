c045429d3e6e935578ce374b95a956161d252fe3
Vit Janota <vitek@Vit--MacBook-Pro.local>
2025-08-14 08:46:25 +0200

    adding more examples and updating index page

--- README.md
+++ README.md
@@ -5,12 +5,18 @@ This is an example of simple notification engine.
 ## Setup
 
 1. add `notifications.js` and `notifications.css` to your code and link them to the page standard way
-2. initialize engine using `let notifications = new Notifications(setup);` where `setup` is a configuration object which can containg any of following propetries:
+2. initialize engine using `let notifications = new Notifications(setup,templates);` where `setup` is a configuration object which can containg any of following propetries:
     - `units`: valid css units definition [units for following properties, default is _px_]
     - `topOffset`: integer [notifications area top margin, in units as defined above, default is _20_]
     - `areaSpacing`: integer [vertical spacing of notifications, in units as defined above, default is _8_]
     - `areaWidth`: positive integer [area width, in units as defined above, default is _300_]
     - `displayDuration`: positive integer [how long should be notification displayed in miliseconds, default is _1500_]
     - `autoHide`: boolean [keep notification displayed or hide it automatically, default is _true_]
-3. evoke functionality like `notifications.emit(message,templates)` where `message` is either a plain string displayed 'as is' or a structured object which can be via `type` property (or _default_ value if not defined) bound to and displayed by (via templating factory) any defined processing template. There are four predefined templates (in `notifications.js`), which can be redefined by `templates` attribute value.
+and `templates` is an object defining custom processing templates (about the usage see further).
+3. evoke functionality like `notifications.emit(message,template)` where `message` is either a plain string displayed 'as is' or a structured object which can be via `template` attribute (or _default_ value if not defined) bound to and displayed by (via templating factory) any defined processing template. There are four predefined templates (in `notifications.js`)
+    - default
+    - warning
+    - error
+    - hint
+which can be extended/redefined during the notifications engine initialization as mentioned above.
 Styling of templates can be defined/altered in `notifications.css` via definitions based on  `div.notificationArea.[template-type]` selector.
--- js/page.js
+++ js/page.js
@@ -1,35 +1,50 @@
-let notifications = new Notifications(
-    {autoHide: true,displayDuration: 1000},
+let notifications = new Notifications();
+let notifications2 = new Notifications(
+    {autoHide: false}
+);
+let notifications3 = new Notifications(
+    {},
     {error: `<h1>My Error Notification: %%title%%</h1>
             <div data-process="body">
                 <span>%%content%%</span>
                 <a href="%%src%%" data-type="link">%%content%%</a>
-            </div>`}
+            </div>`,
+    mytest: `<h1>%%header%%</h1>
+            <ul data-process="list">
+                <li>%%content%%</li>
+            </ul>`}
 );
-let notifications2 = new Notifications(
-    {autoHide: false, areaWidth: 200}
+let notifications4 = new Notifications(
+    {displayDuration: 3000, areaWidth: 150,topOffset:200,areaSpacing:-10}
 );
 
 document.addEventListener("DOMContentLoaded", () => {
     const link = document.getElementById("auto-hide");
     link.addEventListener("click", () => {
         let notif = setTimeout(function(){notifications.emit('Plain text example')},10);
-        notif = setTimeout(function(){notifications.emit('Long long long long long long long long long long long long long long long long long long long long long long long long plain text example')},3000);
-        notif = setTimeout(function(){notifications.emit([{title: 'Default structured mesaage example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}])},5000);
-        notif = setTimeout(function(){notifications.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'error')},9000);
-        notif = setTimeout(function(){notifications.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'warning')},11000);
-        notif = setTimeout(function(){notifications.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'hint')},13000);
-
+        notif = setTimeout(function(){notifications.emit('Long long long long long long long long long long long long long long long long long long long long long long long long plain text example')},1500);
     });
     const link2 = document.getElementById("on-click-hide");
     link2.addEventListener("click", () => {
         let notif = setTimeout(function(){notifications2.emit('Plain text example')},10);
-        notif = setTimeout(function(){notifications2.emit('Long long long long long long long long long long long long long long long long long long long long long long long long plain text example')},500);
-        notif = setTimeout(function(){notifications2.emit([{title: 'Default structured mesaage example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}])},1000);
-        notif = setTimeout(function(){notifications2.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'error')},1500);
-        notif = setTimeout(function(){notifications2.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'warning')},2000);
-        notif = setTimeout(function(){notifications2.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'hint')},2500);
-
+        notif = setTimeout(function(){notifications2.emit('Long long long long long long long long long long long long long long long long long long long long long long long long plain text example')},1500);
+    });
+    const link3 = document.getElementById("predefined-templates");
+    link3.addEventListener("click", () => {
+        let notif = setTimeout(function(){notifications.emit([{title: 'Default structured mesaage example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}])},10);
+        notif = setTimeout(function(){notifications.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'error')},1000);
+        notif = setTimeout(function(){notifications.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'warning')},2000);
+        notif = setTimeout(function(){notifications.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'hint')},3000);
+    });
+    const link4 = document.getElementById("custom-templates");
+    link4.addEventListener("click", () => {
+        let notif = setTimeout(function(){notifications3.emit([{title: 'message example',body: [{content:'This is a '}, {src: '#anchor',content: 'link',type: 'link'},{content: '.'}]}],'error')},10);
+        notif = setTimeout(function(){notifications3.emit([{header: 'my custom message',list: [{content:'item 1'},{content: 'item 2'}]}],'mytest')},1000);
+    });
+    const link5 = document.getElementById("custom-setup");
+    link5.addEventListener("click", () => {
+        let notif = setTimeout(function(){notifications4.emit('Plain text example')},10);
+        notif = setTimeout(function(){notifications4.emit('Long long long long long long long long long long long long long long long long long long long long long long long long plain text example')},1500);
     });
 });
 
